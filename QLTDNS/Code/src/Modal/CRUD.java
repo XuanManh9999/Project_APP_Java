413084d9aea7b42e330ba498b2898a9029706420
Mr Manh Pc <nguyenxuanmanh2992003@gmail.com>
2023-09-30 14:01:39 +0700

    Add base form

---
+++ QLTDNS/Code/src/Modal/CRUD.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Modal;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author nguye
+ */
+public interface CRUD <T>{
+    boolean Create(T obj);
+    ArrayList<T> Read();
+    boolean Update (T obj);
+    boolean Delete(String id);
+    ArrayList<T> Search(String id);
+}
